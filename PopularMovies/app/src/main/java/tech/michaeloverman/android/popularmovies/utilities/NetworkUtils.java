3a621a672a48a63219fa54bd64a9c09241f6c9f4
michaeloverman <overman.michael@gmail.com>
2016-12-08 01:23:12 -0500

    fix api

--- PopularMovies/app/src/main/java/tech/michaeloverman/android/popularmovies/utilities/NetworkUtils.java
+++ PopularMovies/app/src/main/java/tech/michaeloverman/android/popularmovies/utilities/NetworkUtils.java
@@ -25,7 +25,7 @@ public final class NetworkUtils {
     private static final String POSTER_BASE_URL = "http://image.tmdb.org/t/p/";
     private static final String POSTER_SIZE = "w342";
     private static final String API_PARAM = "api_key";
-    private static final String API_KEY = "5b976994bbbba17e3b2e4d2431b5fc2b";
+    private static final String API_KEY = "DELETED FOR SECURITY REASONS";
     private static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
     private static final String POPULAR_PATH = "popular";
     private static final String TOPRATED_PATH = "top_rated";
