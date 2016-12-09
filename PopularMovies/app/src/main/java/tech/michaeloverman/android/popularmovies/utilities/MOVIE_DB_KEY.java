31e6a53ab392443a39ac22a78070a489f637279d
michaeloverman <overman.michael@gmail.com>
2016-12-08 21:59:07 -0500

    still learning about gitignore...

--- PopularMovies/app/src/main/java/tech/michaeloverman/android/popularmovies/utilities/MOVIE_DB_KEY.java
+++ PopularMovies/app/src/main/java/tech/michaeloverman/android/popularmovies/utilities/MOVIE_DB_KEY.java
@@ -5,5 +5,6 @@ package tech.michaeloverman.android.popularmovies.utilities;
  */
 
 final class MOVIE_DB_KEY {
+    // this file has chanced: 2016/12/8 21:58
     public static String KEY = "5b976994bbbba17e3b2e4d2431b5fc2b";
 }
