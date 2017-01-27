308531cf8f33ecfac2790d11299df1bbeb84e3f0
michaeloverman <overman.michael@gmail.com>
2017-01-27 12:15:42 -0500

    functioning, but still putting up two icons in the launcher...

--- GradleProject/jokeslib/src/main/java/com/example/JokeProvider.java
+++ GradleProject/jokeslib/src/main/java/com/example/JokeProvider.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class JokeProvider {
-    private static int currentJoke = 0;
+
     private final static String[] jokes = {
             "A man walks into a bar.\nThe second man ducks.",
             "This is the setup to a bad joke.\nThis is the punchline.........",
@@ -9,8 +9,7 @@ public class JokeProvider {
     };
 
     public static String getJoke() {
-
-        currentJoke = currentJoke++ % jokes.length;
-        return jokes[currentJoke];
+        int joke = (int) (Math.random() * jokes.length);
+        return jokes[joke];
     }
 }
